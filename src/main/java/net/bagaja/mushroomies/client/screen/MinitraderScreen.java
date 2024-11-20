b083ea5715f09d4f82e7096020ea18352d3febe2
niklasT201 <jackvcrackarchive@gmail.com>
2024-11-20 21:58:33 +0100

    changed png

--- src/main/java/net/bagaja/mushroomies/client/screen/MinitraderScreen.java
+++ src/main/java/net/bagaja/mushroomies/client/screen/MinitraderScreen.java
@@ -9,8 +9,8 @@ import net.bagaja.mushroomies.Mushroomies;
 import net.minecraft.client.gui.GuiGraphics;
 
 public class MinitraderScreen extends MerchantScreen {
-    private static final ResourceLocation MINITRADER_GUI_TEXTURE =
-            new ResourceLocation(Mushroomies.MOD_ID, "textures/gui/container/villager.png");
+    private static final ResourceLocation TRADER_INVENTORY =
+            new ResourceLocation(Mushroomies.MOD_ID, "textures/gui/container/trader_inventory.png");
 
     public MinitraderScreen(MerchantMenu menu, Inventory inventory, Component title) {
         super(menu, inventory, title);
@@ -20,6 +20,6 @@ public class MinitraderScreen extends MerchantScreen {
     protected void renderBg(GuiGraphics guiGraphics, float partialTick, int mouseX, int mouseY) {
         int i = (this.width - this.imageWidth) / 2;
         int j = (this.height - this.imageHeight) / 2;
-        guiGraphics.blit(MINITRADER_GUI_TEXTURE, i, j, 0, 0, this.imageWidth, this.imageHeight);
+        guiGraphics.blit(TRADER_INVENTORY, i, j, 0, 0, this.imageWidth, this.imageHeight);
     }
 }
